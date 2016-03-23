3cb412d7134a6ce2a300b72b674d0d34852a4c4e
satomi <satomi@weather-news.co.jp>
2016-03-23 15:12:05 +0900

    Add sub_func() function.

--- a.c
+++ a.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+int sub_func(char* msg)
+{
+  printf("[%s]\n", msg);
+
+  return (0);
+}
+
 int main(int argc, char** argv)
 {
   printf("Hello, world!\n");
+
+  sub_func("OK!");
 
   return (0);
 }
