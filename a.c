b7886514c48eba4968ed7afbe0493520c2b33529
satomi <satomi@weather-news.co.jp>
2016-03-24 13:30:19 +0900

    Add sub_func2() in a.c file.

--- a.c
+++ a.c
@@ -7,11 +7,20 @@ int sub_func(char* msg)
   return (0);
 }
 
+int sub_func2(int val)
+{
+  printf("[%d]\n", val);
+
+  return (0);
+}
+
 int main(int argc, char** argv)
 {
   printf("Hello, world!\n");
 
   sub_func("OK!");
+
+  sub_func2(1999);
 
   return (0);
 }
